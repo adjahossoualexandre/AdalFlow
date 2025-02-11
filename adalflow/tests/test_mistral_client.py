23829bbe1e4bbef87aef2a840b5d704f5fc3cc54
Alexandre Adjahossou <adjahossoualexandre@gmail.com>
2025-02-11 15:14:28 +0100

    Add tests for acall.

--- adalflow/tests/test_mistral_client.py
+++ adalflow/tests/test_mistral_client.py
@@ -1,17 +1,22 @@
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, AsyncMock
 
 from mistralai.models import (
     ChatCompletionResponse,
     UsageInfo,
 )
 
-
 from adalflow.core.types import ModelType, GeneratorOutput
 from adalflow.components.model_client.mistral_client import MistralClient
 
 
-class TestMistralModelClient(unittest.TestCase):
+def getenv_side_effect(key):
+    # This dictionary can hold more keys and values as needed
+    env_vars = {"MISTRAL_API_KEY": "fake_api_key"}
+    return env_vars.get(key, None)  # Returns None if key is not found
+
+
+class TestMistralModelClient(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
         self.client = MistralClient(api_key="fake_api_key")
@@ -38,11 +43,33 @@ class TestMistralModelClient(unittest.TestCase):
             "model": "gpt-3.5-turbo",
         }
 
+    @patch("adalflow.components.model_client.mistral_client.Mistral")
+    async def test_acall_llm(self, MockSyncMistral):
+        # the mistralai library has no dedicated async client. It uses the same client for sync and async.
+        mock_sync_client = Mock()
+        MockSyncMistral.chat.return_value = mock_sync_client
+
+        # Mock the response
+        mock_sync_client.chat.complete_async = AsyncMock(
+            return_value=self.mock_response
+        )
+
+        # Set the sync client
+        self.client.sync_client = mock_sync_client
+        # Mock the client's api: chat.completions.create
+        result = await self.client.acall(
+            api_kwargs=self.api_kwargs, model_type=ModelType.LLM
+        )
+
+        # Assertions
+        mock_sync_client.chat.complete_async.assert_called_once_with(**self.api_kwargs)
+        self.assertEqual(result, self.mock_response)
+
     @patch(
         "adalflow.components.model_client.mistral_client.MistralClient.init_sync_client"
     )
     @patch("adalflow.components.model_client.mistral_client.Mistral")
-    def test_call(self, MockSyncMistral, mock_init_sync_client):
+    def test_call_llm(self, MockSyncMistral, mock_init_sync_client):
         mock_sync_client = Mock()
         MockSyncMistral.return_value = mock_sync_client
         mock_init_sync_client.return_value = mock_sync_client
@@ -72,68 +99,3 @@ class TestMistralModelClient(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-# if __name__ == "__main__":
-#    from adalflow.core import Generator
-#    from adalflow.core import Embedder
-#    from adalflow.utils import setup_env
-#    from adalflow.core.string_parser import JsonParser
-#
-#    setup_env()
-#
-#    # test generation
-#    print("-" * 24)
-#    print("Test Generation pipeline")
-#    print("-" * 24)
-#    rag_template = r"""<START_OF_SYSTEM_MESSAGE>
-# You are a helpful assistant.
-#
-# Your task is to answer the query that may or may not come with context information.
-# When context is provided, you should stick to the context and less on your prior knowledge to answer the query.
-# <END_OF_SYSTEM_MESSAGE>
-# <START_OF_USER_MESSAGE>
-#    <START_OF_QUERY>
-#    {{input_str}}
-#    <END_OF_QUERY>
-#    {% if context_str %}
-#    <START_OF_CONTEXT>
-#    {{context_str}}
-#    <END_OF_CONTEXT>
-#    {% endif %}
-# <END_OF_USER_MESSAGE>
-# """
-#
-#    template = """{{input_str}}"""
-#
-#    model_kwargs = {
-#        "model": "open-mistral-nemo",
-#        "temperature": 1,
-#        "stream": True,
-#    }
-#    prompt_kwargs = {
-#        "input_str": "What is the capital of France?",
-#    }
-#
-#    mistral_client = MistralClient()
-#    generator = Generator(
-#        model_client=mistral_client,
-#        model_kwargs=model_kwargs,
-#        prompt_kwargs=prompt_kwargs,
-#        template=template,
-#    )
-#    output = generator(prompt_kwargs=prompt_kwargs)
-#    print(output)
-#
-#    # test embedding
-#    print("-" * 24)
-#    print("Test Embedding pipeline")
-#    print("-" * 24)
-#    model_kwargs.pop("stream")
-#    model_kwargs.pop("temperature")
-#    model_kwargs["model"] = "mistral-embed"
-#    embedder = Embedder(
-#        model_client=mistral_client,
-#        model_kwargs=model_kwargs,
-#    )
-#    output = embedder(["where is Brian?"])
-#    print(output)
